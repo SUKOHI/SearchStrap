3bfb111519c497041bcf9560161066380dacf500
sukohi <eagle.fly.free0722@gmail.com>
2014-10-28 22:26:03 +0900

    alert.blade.php updated.

--- src/views/alert.blade.php
+++ src/views/alert.blade.php
@@ -1,10 +1,10 @@
 @if(Session::has($level))
 	<div class="alert alert-{{ $level }}{{ ($dismissable) ? ' alert-dismissable' : '' }}">
-		@if(isset($icons[$level]))
-			{{ $icons[$level] }}
-		@endif
 		@if($dismissable)
 			<button type="button" class="close" data-dismiss="alert"><span aria-hidden="true">&times;</span><span class="sr-only">Close</span></button>
+		@endif
+		@if(isset($icons[$level]))
+			{{ $icons[$level] }}
 		@endif
 		{{ Session::get($level) }}
 	</div>
